c2c196376aad679e8a2c81086e9c9da216d79ecd
esmol <smol18@bk.ru>
2021-12-23 10:22:47 +0300

    new commit

--- src/router/index.js
+++ src/router/index.js
@@ -5,38 +5,38 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-		meta: {
-			layout: "AppLayoutDefault",
-		},
-	},
-	{
-		path: "/about",
-		name: "About",
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      layout: "AppLayoutDefault",
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
 
-		component: () => import("../views/About.vue"),
-		meta: {
-			layout: "AppLayoutDefault",
-		},
-	},
-	{
-		path: "/reports",
-		name: "Reports",
+    component: () => import("../views/About.vue"),
+    meta: {
+      layout: "AppLayoutDefault",
+    },
+  },
+  {
+    path: "/reports",
+    name: "Reports",
 
-		component: () => import("../views/Reports.vue"),
-		meta: {
-			layout: "AppLayoutDefault",
-		},
-	},
+    component: () => import("../views/Reports.vue"),
+    meta: {
+      layout: "AppLayoutDefault",
+    },
+  },
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 
 export default router;
